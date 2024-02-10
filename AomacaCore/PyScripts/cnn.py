d1019e11ae0227d8b6be5c34cef40b87f8623fc8
Золотарёв Данил <zolotariov.danil@yandex.ru>
2024-02-10 11:33:28 +0500

    Пока оставил старый файл

--- AomacaCore/PyScripts/cnn.py
+++ AomacaCore/PyScripts/cnn.py
@@ -1,36 +1,8 @@
 import sys
-import numpy as np
-from keras.models import load_model
-from PIL import Image
-
-
-def prepare_image(path):
-    file = Image.open(path).convert("RGB")
-    image_size = (128, 128)
-    return np.array(file.resize(image_size)).flatten() / 255.0
-
-
-def predict_result(path):
-    model = load_model("trained_model.h5")  # load the trained model
-    class_names = ["Forged", "Authentic"]  # classification outputs
-    test_image = prepare_image(path)
-    test_image = test_image.reshape(-1, 128, 128, 3)
-
-    y_pred = model.predict(test_image)
-    y_pred_class = round(y_pred[0][0])
-
-    prediction = class_names[y_pred_class]
-    if y_pred <= 0.5:
-        confidence = f"{(1-(y_pred[0][0])) * 100:0.2f}"
-    else:
-        confidence = f"{(y_pred[0][0]) * 100:0.2f}"
-    return prediction, confidence
 
 
 def PrintResult(path):
-    (prediction, confidence) = predict_result(path)
-    print(f'{prediction}||{confidence}')
-    input()
+    print('00.00')
 
 
 if __name__ == '__main__':
