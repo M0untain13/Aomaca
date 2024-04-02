06fba9bdd53f476d7e346d2365d28fa003b02cd0
M0untain13 <dan2002.dan2002@mail.ru>
2024-04-02 15:42:41 +0500

    Добавил более быстродействующий скрипт в папку new, но для него нужно тренеровать нейросеть

--- AomacaCore/PyFiles/New/ela.py
+++ AomacaCore/PyFiles/New/ela.py
@@ -1,8 +1,5 @@
 import sys
-from PIL import Image, ImageChops, ImageEnhance
-
-import numpy as np
-from sklearn.decomposition import PCA
+from PIL import Image, ImageChops, ImageEnhance, ImageFilter
 
 
 def CreateElaImage(path: str, quality: int) -> None:
@@ -10,31 +7,25 @@ def CreateElaImage(path: str, quality: int) -> None:
     resavedFilePath = f"{baseDir}\\resaved_image.jpg"
     elaFilePath = f"{baseDir}\\ela_image.png"
 
-    # Resave image
-    originalImage = Image.open(path).convert('L')
+    originalImage = Image.open(path).convert("L")
     originalImage.save(resavedFilePath, "JPEG", quality=quality)
     resavedImage = Image.open(resavedFilePath)
 
-    # Noice
     elaImage = ImageChops.difference(originalImage, resavedImage)
 
-    # Extract high frequency noise
-    noise_flat = np.array(elaImage)
-    pca = PCA(n_components=5)
-    image_compressed = pca.fit_transform(noise_flat)
+    elaImage = elaImage.filter(ImageFilter.GaussianBlur(radius=4))
 
-    image_decompressed = pca.inverse_transform(image_compressed)
-
-    high_noise = Image.fromarray(image_decompressed).convert('L')
-
-    extrema = high_noise.getextrema()  # Коэффициенты масштабирования рассчитываются по экстремумам пикселей
+    extrema = elaImage.getextrema()  # Коэффициенты масштабирования рассчитываются по экстремумам пикселей
     maxDifference = max(extrema)
     if maxDifference == 0:
         maxDifference = 1
     scale = 350.0 / maxDifference
-    high_noise = ImageEnhance.Brightness(high_noise).enhance(scale)  # Выравниваем яркость изображения
+    print(scale)
+    if scale > 25:
+        scale = 25
+    elaImage = ImageEnhance.Brightness(elaImage).enhance(scale)  # Выравниваем яркость изображения
 
-    high_noise.save(elaFilePath)
+    elaImage.save(elaFilePath)
 
 
 def Main(args: list) -> None:
